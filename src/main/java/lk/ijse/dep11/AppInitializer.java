33057f1815838559b09fcb110829efc5b9530cf2
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-09-24 20:23:01 +0530

    Add New Employee

---
+++ src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -0,0 +1,22 @@
+package lk.ijse.dep11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MainView.fxml"))));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Simple App");
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+    }
+}
--- src/main/java/lk/ijse/dep11/controller/MainViewController.java
+++ src/main/java/lk/ijse/dep11/controller/MainViewController.java
@@ -1,4 +1,76 @@
 package lk.ijse.dep11.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.dep11.tm.Employee;
+
+import java.util.ArrayList;
+
 public class MainViewController {
+    public AnchorPane root;
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtContact;
+    public TableView<Employee> tblEmployee;
+    public Button btnSave;
+    public Button btnDelete;
+    public Button btnAddNewEmployee;
+
+    public ArrayList<Employee> employeesList = new ArrayList<>();
+    public TextField txtSearch;
+
+    public ObservableList<Employee> employeesObservablList;
+    public  void initialize(){
+        tblEmployee.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblEmployee.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblEmployee.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("contact"));
+
+        for(Control control: new Control[]{txtName,txtContact,btnDelete,btnSave}){
+            control.setDisable(true);
+        }
+        btnAddNewEmployee.requestFocus();
+
+        //employeesList = readEmployee();
+        employeesList.add(new Employee("E-001","Shan","+9477-6589898"));
+        employeesObservablList = FXCollections.observableList(employeesList);
+        tblEmployee.setItems(employeesObservablList);
+
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddNewEmployeeOnAction(ActionEvent actionEvent) {
+        txtName.setDisable(false);
+        txtContact.setDisable(false);
+        btnSave.setDisable(false);
+        txtName.requestFocus();
+        txtID.setText(getID());
+    }
+    String getID(){
+        if(employeesList.isEmpty())return "E-001";
+        return String.format("E-%03d",Integer.parseInt(employeesList.get(employeesList.size()-1).getId().substring(2))+1);
+    }
+
+    public void tblEmployeeOnDragDetected(MouseEvent event) {
+    }
+
+    public void tblEmployeeOnDragOver(DragEvent dragEvent) {
+    }
+
+    public void tblEmployeeOnDragDropped(DragEvent dragEvent) {
+    }
+
 }
